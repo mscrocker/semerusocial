28b1207a0488f09e3593f46817a5e302ceb41b8a
alex <alejandro.garcia.rodriguez@udc.es>
2019-09-22 20:47:23 +0200

    añadida la clave foranea de imagetable al changelog.xml

--- src/main/java/es/udc/fi/dc/fd/controller/SecurityConfig.java
+++ src/main/java/es/udc/fi/dc/fd/controller/SecurityConfig.java
@@ -1,59 +1,28 @@
 package es.udc.fi.dc.fd.controller;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import es.udc.fi.dc.fd.jwt.JwtFilter;
 import es.udc.fi.dc.fd.jwt.JwtGenerator;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter  {
-	
-	
-	private JwtGenerator jwtGenerator;
-	
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
-	public SecurityConfig(JwtGenerator jwtGenerator){
-		super();
-		
-        this.jwtGenerator = checkNotNull(jwtGenerator,
-                "Received a null pointer as jwtGenerator");
-        
-		
-	}
-	
-	 @Override
-	 public AuthenticationManager authenticationManagerBean() throws Exception {
-	     return super.authenticationManagerBean();
-	 }
+	private JwtGenerator jwtGenerator;
 	
 	@Override
 	   protected void configure(HttpSecurity httpSecurity) throws Exception {
-	
-			httpSecurity.cors().and().csrf().disable()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.addFilter(new JwtFilter(authenticationManagerBean(), jwtGenerator))
-			.authorizeRequests()
-			.antMatchers("/**").permitAll();
+			httpSecurity.authorizeRequests().antMatchers("/").permitAll();
 	}
-	
-
 	
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
--- src/main/java/es/udc/fi/dc/fd/model/persistence/ImageImpl.java
+++ src/main/java/es/udc/fi/dc/fd/model/persistence/ImageImpl.java
@@ -6,10 +6,13 @@ import java.util.Arrays;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -37,6 +40,7 @@ public class ImageImpl implements Image {
     /**
      * Entity's userId.
      */
+
 	@JoinColumn(name="userId", referencedColumnName="userId")
     private User user = null;
 
