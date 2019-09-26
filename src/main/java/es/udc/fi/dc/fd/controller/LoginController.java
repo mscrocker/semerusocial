858daca43f6c7115239473a81c80ae4ac7a420bc
Ishwara Coello Escobar <ishwara.coello@udc.es>
2019-09-27 01:19:07 +0200

    Controllador añadido para mostrar formularios de login y sign up

--- src/main/java/es/udc/fi/dc/fd/controller/LoginController.java
+++ src/main/java/es/udc/fi/dc/fd/controller/LoginController.java
@@ -1,27 +1,19 @@
 package es.udc.fi.dc.fd.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
 public class LoginController {
 
-	private static final String VIEW_LOGIN = "login";
-	
-	public LoginController() {
-		super();
+	@GetMapping(path = "/login")
+	public final String displayLogin() {
+		return UserViewConstants.LOGIN_FORM;
 	}
-	
-	/**
-     * Shows the login view.
-     * 
-     * @return the login view
-     */
-    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
-	public final String showLoginForm() {
-		return VIEW_LOGIN;
+
+	@GetMapping(path = "/signup")
+	public final String displaySignUpForm() {
+		return UserViewConstants.SIGN_UP_FORM;
 	}
+
 }
---
+++ src/main/java/es/udc/fi/dc/fd/controller/UserViewConstants.java
@@ -0,0 +1,14 @@
+package es.udc.fi.dc.fd.controller;
+
+public final class UserViewConstants {
+	public static final String LOGIN_FORM = "login";
+	public static final String SIGN_UP_FORM = "signUp";
+
+	/**
+	 * Private constructor to avoid initialization.
+	 */
+	private UserViewConstants() {
+		super();
+	}
+
+}
