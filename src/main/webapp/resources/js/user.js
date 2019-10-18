b02b5930d9b9f5cf9a70e60789d503010071d122
Marco Martínez Sánchez <marco.msanchez@udc.es>
2019-10-18 20:56:00 +0200

    Moved all the javascript functions and objects that could be used as a module to its own namespace.
    This commit refs #77

--- src/main/webapp/resources/js/user.js
+++ src/main/webapp/resources/js/user.js
@@ -1,29 +1,32 @@
-const updateLoggedIn = (loginButtonName, userURL) => {
-	if ((sessionStorage.user_jwt === undefined) || (sessionStorage.user_name === undefined)){
-		return;
-	}
-	document.getElementById(loginButtonName).innerText = sessionStorage.user_name;
-	document.getElementById(loginButtonName).formAction = userURL;
-};
-
-const authFetch = (url, params, onSuccess, onErrors) => {
-	if (sessionStorage.user_jwt !== undefined){
-		if (params.headers === undefined){
-			params.headers = {};
+const user = {
+	updateLoggedIn: (loginButtonName, userURL) => {
+		if ((sessionStorage.user_jwt === undefined) || (sessionStorage.user_name === undefined)){
+			return;
 		}
-		
-		params.headers.Authorization = 'Bearer ' + sessionStorage.user_jwt;
+		document.getElementById(loginButtonName).innerText = sessionStorage.user_name;
+		document.getElementById(loginButtonName).formAction = userURL;
+	},
+
+	authFetch: (url, params, onSuccess, onErrors) => {
+		if (sessionStorage.user_jwt !== undefined){
+			if (params.headers === undefined){
+				params.headers = {};
+			}
+			
+			params.headers.Authorization = 'Bearer ' + sessionStorage.user_jwt;
+		}
+		fetch(url, params)
+		.then((response) => onSuccess(response))
+		.catch((errors) => onErrors(errors));
+	},
+
+	checkLoggedIn: () => {
+		return sessionStorage.user_jwt !== undefined;
+	},
+
+	logOut: () => {
+		delete sessionStorage.user_jwt;
+		delete sessionStorage.user_name;
 	}
-	fetch(url, params)
-	.then((response) => onSuccess(response))
-	.catch((errors) => onErrors(errors));
 };
 
-const checkLoggedIn = () => {
-	return sessionStorage.user_jwt !== undefined;
-};
-
-const logOut = () => {
-	delete sessionStorage.user_jwt;
-	delete sessionStorage.user_name;
-};
