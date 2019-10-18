32c15e524cc1ac0d9119bcacd8b79a6c04440941
Ishwara Coello Escobar <ishwara.coello@udc.es>
2019-10-19 00:04:25 +0200

    37: Handler for next and previous buttons added
    Task-Url: https://redmine.fic.udc.es/issues/37
    This commit refs:#37 @1h20m

--- src/main/webapp/resources/js/friendSuggestion.js
+++ src/main/webapp/resources/js/friendSuggestion.js
@@ -8,20 +8,26 @@ const initSuggestion = (Url) => {
     	let controller = document.getElementById("imagesController");
     	if (controller.childElementCount < PAGE_SIZE) {
     		askForMore = false;
-    		//    		Do Something special when user doens't have any image
+    		// Do Something special when user doens't have any image
     	}
     	if (controller.childElementCount === 0) {
-    		//Do Something special when user doens't have any image
+    		// Do Something special when user doens't have any image
     	}
     	
     	else {
     		controller.firstElementChild.classList.add("active");
-    		//counter++;
+    		// counter++;
     	}
     	
-}
+};
 const loadImages = (baseUrl) => {
-
+    	// Show a spinner while we load it
+	let spinner = document.getElementById("loader");
+	// Hide the next button till we process the request
+	let next = document.getElementById("nextButton");
+	 next.classList.add("hidden");
+	spinner.classList.remove("hidden");
+	 // Todo , get userId of suggestedUser
 	const url = baseUrl + "backend/images/carrusel/user/1" + "?page="+ page;
 	authFetch(url, {
 		method: 'GET'
@@ -33,18 +39,16 @@ const loadImages = (baseUrl) => {
 		}
 		response.json().then((body) => {
 				
-//			//block -> {
-//			long:imageId
-//			String data;
-//			String type;
-//		}
+
 			let images = body.images;
 			let exists = body.existMoreImages;
+			// If we dont have more images we make sure we dont make
+			// more petitions
 			if (!exists) 
 				askForMore = false;
 			
 	    	let controller = document.getElementById("imagesController");
-    		
+    		// Add the images to the DOM
 	     	images.forEach( image => {
 	    		let div = document.createElement("div");
 	    		let img = document.createElement("img");
@@ -55,13 +59,18 @@ const loadImages = (baseUrl) => {
 	    		div.append(img);
 	    		controller.append(div);
 	    	});
+	     	// Only if we received more images we proceed
+	     	// Move to the next image in case we had at least one more
 	     	if (images.length >= 1){
 	    		controller.children[counter].classList.remove("active");
-	controller.children[++counter].classList.add("active");
-
+	    		controller.children[++counter].classList.add("active");
+	    		
+	    		if (images.length !== 1){
+	    			next.classList.remove("hidden");
+		     	}
 	    	}
 			// data + type -> string
-			
+			spinner.classList.add("hidden");
 		});
 		
 	}, (errors) => {
@@ -70,33 +79,50 @@ const loadImages = (baseUrl) => {
 };
 const onNextPressed = (baseUrl) => {
 	let controller = document.getElementById("imagesController");
+	// Case when we move from the first Slide
+	if (counter=== 0){
+		let prev = document.getElementById("previousButton");
+		prev.classList.remove("hidden");
+	}
+	// Case when we are returning to the last slide and we know is the last
+	// one
+	if (counter+2 == controller.childElementCount && !askForMore){
+		let next = document.getElementById("nextButton");
+	next.classList.add("hidden");
+}
+	// When we reach the last element
 	if (controller.childElementCount === counter+1 && true) {
 		if(!askForMore)
 			return;
-		let spinner = document.getElementById("loader");
-		spinner.classList.remove("hidden");
 		page++;
 		loadImages(baseUrl);
-		spinner.classList.add("hidden");
 
-		//Fetch next page and append those images to the carrousel;
-		//Show also a spinner in the meanwhile?
+		// Fetch next page and append those images to the carrousel;
+		// Show also a spinner in the meanwhile?
 		
 	}
 	else {
 
-	//At This poiunt we are sure we have more elements
+	// At This poiunt we are sure we have more elements
 	controller.children[counter].classList.remove("active");
 	controller.children[++counter].classList.add("active");
 	}
-}
+};
 
 
 const onPrevPressed = () => {
 	let controller = document.getElementById("imagesController");
-	if (counter === 0) {
-		return;
+	// Case where we are returning to the initial slide
+	if (counter === 1  ) {
+		let prev = document.getElementById("previousButton");
+		prev.classList.add("hidden");
+
+	}
+	// Case where we are coming from the last slide
+	if (counter === controller.childElementCount -1  ) {
+		let next = document.getElementById("nextButton");
+		next.classList.remove("hidden");
 	}
 	controller.children[counter].classList.remove("active");
 	controller.children[--counter].classList.add("active");
-}
+};
