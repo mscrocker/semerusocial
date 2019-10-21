26cf9da15a14c314258df63f2428b823079adb1a
Marco Martínez Sánchez <marco.msanchez@udc.es>
2019-10-21 19:37:40 +0200

    Fix: missing default criteria constants

---
+++ src/main/java/es/udc/fi/dc/fd/service/DefaultCriteria.java
@@ -0,0 +1,9 @@
+package es.udc.fi.dc.fd.service;
+
+import es.udc.fi.dc.fd.model.SexCriteriaEnum;
+
+public class DefaultCriteria {
+	public static final int DEFAULT_CRITERIA_MIN_AGE = 18;
+	public static final int DEFAULT_CRITERIA_MAX_AGE = 99;
+	public static final SexCriteriaEnum DEFAULT_CRITERIA_SEX = SexCriteriaEnum.ANY;
+}
