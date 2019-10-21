4aa1815b612e1af730f90147826cb5c9ba519244
Marco Martínez Sánchez <marco.msanchez@udc.es>
2019-10-21 12:57:07 +0200

    Added elemental ability to update user profile data
    This commit refs #38 @1h
    Task-Url: https://redmine.fic.udc.es/issues/38

--- src/main/webapp/resources/js/profile.js
+++ src/main/webapp/resources/js/profile.js
@@ -1,14 +1,86 @@
 const profile = {
 
 	UserData: {
-				'age' : null,
-			'sex' : null, 
-			'city' : null
+		'age' : null,
+		'sex' : null, 
+		'city' : null
+	},
+	
+	notifyChange: () => {
+		document.getElementById("profileButton").disabled = false;
+	},
+	
+	clearFeedback: (name) => {
+		document.getElementById(name).classList.remove("is-valid");
+		document.getElementById(name).classList.remove("is-invalid");
+	},
+	
+
+	validateFields: () => {
+		let age = document.getElementById("ageField").value;
+		let sex = document.getElementById("sexField").value;
+		let city = document.getElementById("cityField").value;
+		let description = document.getElementById("descriptionField").value;
+		
+		
+		profile.clearFeedback("ageField");
+		profile.clearFeedback("descriptionField");
+		profile.clearFeedback("cityField");
+		profile.clearFeedback("sexField");
+		
+		
+		document.getElementById("ageField").classList.add("is-valid");
+		document.getElementById("descriptionField").classList.add("is-valid");
+		document.getElementById("cityField").classList.add("is-valid");
+		document.getElementById("sexField").classList.add("is-valid");
+		return true;
+	},
+	
+
+	updateCriteria: () => {
+		document.getElementById("profileButton").disabled = true;
+		if (profile.validateFields()){
+			//TODO: undo mocked date
+			let date = new Date(Date.now());
+			let profile = {
+				//age: document.getElementById("ageField").value,
+				day: date.getUTCDay(),
+				month: date.getUTCMonth(),
+				year: date.getUTCFullYear() - document.getElementById("ageField").value,
+					
+				
+				description: document.getElementById("descriptionField").value,
+				city: document.getElementById("cityField").value,
+				sex: document.getElementById("sexField").value
+			};
+			
+			let url = criteriaSettings.baseURL + "backend/users/updateProfile";
+			let params = {
+				method: 'PUT',
+				body: JSON.stringify(profile),
+				headers: { "Content-Type": "application/json" }
+			};
+			user.authFetch(url, params, (response) => {
+				if (response.status !== 204){
+					showAlert("Internal server error");
+				}
+			}, (errors) => {
+				showAlert("Internal server error");
+			});
+		}
 	},
 
 	initProfile: (baseURL) => {
 		const url = baseURL + "backend/users/data" ;
 		if (user.checkLoggedIn()){
+			
+			document.getElementById("ageField").onclick = profile.notifyChange;
+			document.getElementById("descriptionField").onclick = profile.notifyChange;
+			document.getElementById("sexField").onclick = profile.notifyChange;
+			document.getElementById("cityField").onclick = profile.notifyChange;
+			
+			document.getElementById("profileButton").onclick = profile.updateCriteria;
+			
 			user.authFetch(url, {method: 'GET'}, (response) => {	
 				if (response.status !== 200){
 					console.log("ERROR!");
