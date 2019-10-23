f8b3767a7890270912ac0b7348df899f8cac72e5
Marco Martínez Sánchez <marco.msanchez@udc.es>
2019-10-23 23:24:51 +0200

    Completed search criteria and profile data edition screen
    This commit refs #38 @5h8m
    Task-Url: https://redmine.fic.udc.es/issues/38

--- src/main/webapp/resources/js/profile.js
+++ src/main/webapp/resources/js/profile.js
@@ -1,14 +1,38 @@
-const profile = {
+let profile = null;
+ profile = {
 
 	UserData: {
-		'age' : null,
+		'birthDate' : null,
 		'sex' : null, 
 		'city' : null,
 		'description': null
 	},
 	
-	notifyChange: () => {
-		document.getElementById("profileButton").disabled = false;
+	clearStatusIcons: () => {
+		document.getElementById("profileLoading").classList.add("hidden");
+		document.getElementById("profileFinished").classList.add("hidden");
+		document.getElementById("profileError").classList.add("hidden");
+	},
+	
+	notifyChange: (name) => {
+		profile.clearStatusIcons();
+		let result = false;
+		switch (name) {
+			case "birthDate": 
+				result = profile.validation.validateBirthDate(true);
+				break;
+			case "description":
+				result = profile.validation.validateDescription(true);
+				break;
+			case "sex":
+				result = profile.validation.validateSex(true);
+				break;
+			case "city":
+				result = profile.validation.validateCity(true);
+				break;
+		}
+
+		document.getElementById("profileButton").disabled = !result;
 	},
 	
 	clearFeedback: (name) => {
@@ -17,70 +41,98 @@ const profile = {
 	},
 	
 
-	validateFields: () => {
-		let age = document.getElementById("ageField").value;
-		let sex = document.getElementById("sexField").value;
-		let city = document.getElementById("cityField").value;
-		let description = document.getElementById("descriptionField").value;
+	validation: {
+		clearAllFields: () => {
+			validation.clearField(document.getElementById("birthDateField"));
+			validation.clearField(document.getElementById("sexField"));
+			validation.clearField(document.getElementById("cityField"));
+			validation.clearField(document.getElementById("descriptionField"));
+		},
 		
+		validateBirthDate: (updateField) => {
+			let age = new Date(new Date() - new Date(document.getElementById("birthDateField").value)).getFullYear() - 1970;
+			let result = (age >= 18);
+			if (updateField === true){
+				validation.updateField(document.getElementById("birthDateField"), result);
+			}
+			return result;
+		},
 		
-		profile.clearFeedback("ageField");
-		profile.clearFeedback("descriptionField");
-		profile.clearFeedback("cityField");
-		profile.clearFeedback("sexField");
+		validateDescription: (updateField) => {
+			let description = document.getElementById("descriptionField").value;
+			let result = (description !== "") && (description !== undefined) && (description !== null);
+			if (updateField === true){
+				validation.updateField(document.getElementById("descriptionField"), result);
+			}
+			return result;
+		},
 		
+		validateSex: (updateField) => {
+			let sex = document.getElementById("sexField").value;
+			let result = ["Male", "Female", "Other"].includes(sex);
+			if (updateField === true){
+				validation.updateField(document.getElementById("sexField"), result);
+			}
+			return result;
+		},
 		
-		document.getElementById("ageField").classList.add("is-valid");
-		document.getElementById("descriptionField").classList.add("is-valid");
-		document.getElementById("cityField").classList.add("is-valid");
-		document.getElementById("sexField").classList.add("is-valid");
-		return true;
+		validateCity: (updateField) => {
+			let city = document.getElementById("cityField").value;
+			let result = (city !== "") && (city !== undefined) && (city !== null);
+			if (updateField === true){
+				validation.updateField(document.getElementById("cityField"), result);
+			}
+			return result;
+		}
 	},
 	
 
 	updateCriteria: () => {
 		document.getElementById("profileButton").disabled = true;
-		if (profile.validateFields()){
-			//TODO: undo mocked date
-			let date = new Date(Date.now());
-			let profile = {
-				//age: document.getElementById("ageField").value,
-				day: date.getUTCDay(),
-				month: date.getUTCMonth(),
-				year: date.getUTCFullYear() - document.getElementById("ageField").value,
-					
-				
-				description: document.getElementById("descriptionField").value,
-				city: document.getElementById("cityField").value,
-				sex: document.getElementById("sexField").value
-			};
+		profile.clearStatusIcons();
+		let date = new Date(document.getElementById("birthDateField").value);
+		let profileData = {
+			day: date.getUTCDate(),
+			month: date.getUTCMonth(),
+			year: date.getUTCFullYear(),
+			description: document.getElementById("descriptionField").value,
+			city: document.getElementById("cityField").value,
+			sex: document.getElementById("sexField").value
+		};
+		
+		let url = criteriaSettings.baseURL + "backend/users/updateProfile";
+		let params = {
+			method: 'PUT',
+			body: JSON.stringify(profileData),
+			headers: { "Content-Type": "application/json" }
+		};
+		user.authFetch(url, params, (response) => {
+			profile.validation.clearAllFields();
+			if (response.status !== 204){
+				customAlert.showAlertFromResponse(response);
+				profile.clearStatusIcons();
+				document.getElementById("profileError").classList.remove("hidden");
+			} else {
+				profile.clearStatusIcons();
+				document.getElementById("profileFinished").classList.remove("hidden");
+			}
 			
-			let url = criteriaSettings.baseURL + "backend/users/updateProfile";
-			let params = {
-				method: 'PUT',
-				body: JSON.stringify(profile),
-				headers: { "Content-Type": "application/json" }
-			};
-			user.authFetch(url, params, (response) => {
-				if (response.status !== 204){
-					showAlert("Internal server error");
-				}
-			}, (errors) => {
-				showAlert("Internal server error");
-			});
-		}
+		}, (errors) => {
+			customAlert.showAlert(errors);
+		});
 	},
 
 	initProfile: (baseURL) => {
+		profile.clearStatusIcons();
 		const url = baseURL + "backend/users/data" ;
 		if (user.checkLoggedIn()){
 			
-			document.getElementById("ageField").onclick = profile.notifyChange;
-			document.getElementById("descriptionField").onclick = profile.notifyChange;
-			document.getElementById("sexField").onclick = profile.notifyChange;
-			document.getElementById("cityField").onclick = profile.notifyChange;
+			document.getElementById("birthDateField").onchange = () => profile.notifyChange("birthDate");
+			document.getElementById("descriptionField").onchange = () => profile.notifyChange("description");
+			document.getElementById("sexField").onclick = () => profile.notifyChange("sex");
+			document.getElementById("cityField").onclick = () => profile.notifyChange("city");
 			
-			document.getElementById("profileButton").onclick = profile.updateCriteria;
+			document.getElementById("profileButton").onclick = () => profile.updateCriteria();
 			
 			user.authFetch(url, {method: 'GET'}, (response) => {	
 				if (response.status !== 200){
@@ -88,12 +140,16 @@ const profile = {
 					return;
 				}
 				response.json().then((body) => {
-					profile.UserData.age = body.age;
+					profile.UserData.birthDate = new Date();
+					profile.UserData.birthDate.setUTCFullYear(body.date.year);
+					profile.UserData.birthDate.setUTCMonth(body.date.monthValue - 1);
+					profile.UserData.birthDate.setUTCDate(body.date.dayOfMonth);
+					
 					profile.UserData.sex = body.sex;
 					profile.UserData.city = body.city;
 					profile.UserData.description = body.description;
 					
-					document.getElementById("ageField").value = ""+body.age;
+					document.getElementById("birthDateField").value = profile.UserData.birthDate.toISOString().substr(0,10);
 					document.getElementById("cityField").value = ""+body.city;
 					document.getElementById("sexField").value = ""+body.sex;
 					document.getElementById("descriptionField").value = ""+body.description;
