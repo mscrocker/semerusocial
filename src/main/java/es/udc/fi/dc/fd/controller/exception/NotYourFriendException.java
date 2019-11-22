453b32933187e823896b52a18f1d0bc676b7efc6
mirian.martinez.alvarez <mirian.martinez.alvarez@udc.es>
2019-11-22 16:23:11 +0100

    267: ChatService
    Task-Url: https://redmine.fic.udc.es/issues/267

--- src/main/java/es/udc/fi/dc/fd/controller/exception/NotYourFriendException.java
+++
@@ -1,9 +0,0 @@
-package es.udc.fi.dc.fd.controller.exception;
-
-@SuppressWarnings("serial")
-public class NotYourFriendException extends Exception {
-
-	public NotYourFriendException(String message) {
-		super(message);
-	}
-}
