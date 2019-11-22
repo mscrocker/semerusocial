2e02ae62a60495bd2172283047ad09b8cac99cd2
ishwara Coello <ishwara.coello@udc.es>
2019-11-22 18:09:28 +0100

    272: Friend List sorted and date formatting done
    Task-Url: https://redmine.fic.udc.es/issues/272
    This task refs: #272 @2h17m

--- src/main/webapp/resources/js/chat.js
+++ src/main/webapp/resources/js/chat.js
@@ -1,23 +1,25 @@
+/*jshint esversion: 6 */
+
 const chat = {
 	conversation: null, // element where messages are visible
 	baseURL: null, // To make petitions to backend
-	scrollableDiv: null,  // Scroll elemennt on chat
+	scrollableDiv: null, // Scroll elemennt on chat
 	buttonCollapse: null, // Button to collapse sidebar
 	friendScroll: null, // Scroll element on friendList
 	loadPrevious: true, // Whether we know there are previous messages or
-			    // not
+	// not
 	loadMoreFriends: true, // Whether we know there are more friends or not
 	friendList: [], // List of friends
 	chatMessage: {}, // Chat message
-			    // {messages:[],scroll:x,lastActivePage:y,needMoreLoading:true/false}
+	// {messages:[],scroll:x,lastActivePage:y,needMoreLoading:true/false}
 	activeFriend: 0, // Id of active friend
 	currentMessagePage: 0, // Page of current fetched chat
-	friendListPage: 0,  // Page of current fetched friends
-	
+	friendListPage: 0, // Page of current fetched friends
+
 	// Called on paged load;
 	init: (baseURL) => {
-	    	// Module variables initialization
-	    	chat.baseURL = baseURL;
+		// Module variables initialization
+		chat.baseURL = baseURL;
 		chat.buttonCollapse = document.getElementById("backArrow");
 		chat.conversation = document.querySelector('.conversation-container');
 		chat.friendList = document.getElementById("friendHolder");
@@ -31,25 +33,25 @@ const chat = {
 				apis.fetchFriendMessages(chat.activeFriend);
 			}
 		});
-		
+
 		// Automatically collapses the sidebar on resize;
 		$(window).resize(function () {
-				if (document.documentElement.clientWidth <= 761) {
-					if (!$('#sidebar, #content')
-						.hasClass("active")) {
-						chat.buttonPressed();
-					}
+			if (document.documentElement.clientWidth <= 761) {
+				if (!$('#sidebar, #content')
+					.hasClass("active")) {
+					chat.buttonPressed();
 				}
-			});
+			}
+		});
 		$('#sidebarCollapse')
 			.on('click', chat.buttonPressed);
 
 		// Handler for creating your messages
 		document.querySelector('.conversation-compose').addEventListener('submit', messages.newMessage);
-		
+
 		// init page
 		chat.initScreen();
-		
+
 	},
 	// Collapse Sidebar Function
 	buttonPressed: () => {
@@ -63,50 +65,78 @@ const chat = {
 			.attr('aria-expanded', 'false');
 	},
 	initScreen: () => {
-	   
-	    apis.loadFriendList();
-	    let id = window.location.pathname.split("/").pop();
-	    if (!isNaN(parseFloat(id)) && !isNaN(id - 0)){
-		chat.activeFriend = id;
-	    }
-	    else {
-		chat.activeFriend = chat.friendList.firstElementChild.getAttribute("data-id");
-	    }
 
-		chat.chatMessage[chat.activeFriend] = { messages: [], scroll: 0,needMoreLoading: true,lastActivePage:0};
+		apis.loadFriendList();
+		let id = window.location.pathname.split("/").pop();
+		if (!isNaN(parseFloat(id)) && !isNaN(id - 0)) {
+			chat.activeFriend = id;
+		} else {
+			chat.activeFriend = chat.friendList.firstElementChild.getAttribute("data-id");
+		}
+
+		chat.chatMessage[chat.activeFriend] = {
+			messages: [],
+			scroll: 0,
+			needMoreLoading: true,
+			lastActivePage: 0
+		};
 		apis.fetchFriendMessages(chat.activeFriend);
+	},
+	formatDate: (date) => {
+		let now = moment();
+		if (date.isSame(now, "day"))
+			return date.format("HH:mm");
+
+		now = now.startOf("day").add(-1, "day");
+		if (date.isSame(now, "day"))
+			return "Yesterday";
+		now = now.add(-6, "day");
+		if (date.isAfter(now))
+			return date.format("dddd");
+
+		return date.format("L");
 	}
 };
 
 let friends = {
-	addFriendToList: (friend) => {
-
+	addFriendToList: ({
+		lastMessage,
+		name,
+		id,
+		count,
+		date
+	}) => {
+		// Friend already on list
+		if (document.querySelector(`li[data-id="${id}"]`)) {
+			return;
+		}
 		let li = document.createElement('li');
-		let a= document.createElement('div');
-		let imgSpan = document.createElement('img');
+		let a = document.createElement('div');
+		let imgSpan = document.createElement('span');
+		let lineContainer = document.createElement('div');
+
 		let div1 = document.createElement('div');
 		let div2 = document.createElement('div');
-		
+
 		let titleSpan = document.createElement('div');
 		let userDate = document.createElement('div');
-		let notis= document.createElement('span');
+		let notis = document.createElement('span');
 		let message = document.createElement('p');
-		
-		message.innerText = friend.lastMessage;
-		
-		if (friend.count && friend.count !== 0) {
-		    notis.innerText = friend.count;
-		    
+
+		message.innerText = lastMessage;
+
+		if (count && count !== 0) {
+			notis.innerText = count;
+
+		} else {
+			notis.classList.add("hidden");
 		}
-		else {
-		    notis.classList.add("hidden");
-		}
-		
-		
-		titleSpan.innerText = friend.name;
-		userDate.innerText = "10-10-1998 04:04";
-		
-		
+
+
+		titleSpan.innerText = name;
+		userDate.innerText = chat.formatDate(date);
+
+
 		li.appendChild(a);
 		a.appendChild(imgSpan);
 		div1.appendChild(titleSpan);
@@ -114,76 +144,90 @@ let friends = {
 
 		div2.appendChild(message);
 		div2.appendChild(notis);
-		a.appendChild(div1);
-		a.appendChild(div2);
+		lineContainer.appendChild(div1);
+		lineContainer.appendChild(div2);
+		a.appendChild(lineContainer);
 
-		
-		
+
 		// li.setAttribute('data-userid', friend.id);
 		a.setAttribute('class', 'friend');
 		a.setAttribute('data-toggle', 'collapse');
 		a.setAttribute('aria-expanded', 'false');
 
 		a.onclick = () => {
-			if (friend.id != chat.activeFriend) {
-				console.log(friend.id);
+			if (id != chat.activeFriend) {
+				console.log(id);
+				document.querySelector(`li[data-id="${id}"] .line2 .notis`).classList.add("hidden");
+				document.querySelector(`[data-id="${id}"]`).firstElementChild.classList.add("clicked");
 				document.querySelector(`[data-id="${chat.activeFriend}"]`).firstElementChild.classList.remove("clicked");
-				document.querySelector(`[data-id="${friend.id}"]`).firstElementChild.classList.add("clicked");
-				friends.switchToUser(friend.id);
-				
-				
+				friends.switchToUser(id);
+
+
 			}
 		};
 
-		
-		div1.setAttribute('class',"line1");
-		div2.setAttribute('class',"line2");
-		userDate.setAttribute('class',"user-date");
-		notis.setAttribute('class',"notis user-circle");
-		
+		lineContainer.setAttribute('class', "user-wrapper");
+		div1.setAttribute('class', "line1");
+		div2.setAttribute('class', "line2");
+		userDate.setAttribute('class', "user-date");
+		notis.setAttribute('class', "notis user-circle");
+
 		imgSpan.setAttribute('class', 'user-img');
 		titleSpan.setAttribute('class', 'user-title');
-		li.setAttribute('data-id', friend.id);
+		li.setAttribute('data-id', id);
+		li.setAttribute('data-date', date.toISOString());
 
 
 		message.setAttribute('class', 'user-desc');
-		chat.friendList.appendChild(li);
-	
+		let liCount = chat.friendList.childElementCount;
+		for (i = 0; i < liCount; i++) {
+			let elemDate = chat.friendList.children[i].getAttribute("data-date");
+			if (date.toISOString() > elemDate) {
+				chat.friendList.insertBefore(li, chat.friendList.children[i]);
+				break;
+			}
+		}
+		if (liCount === chat.friendList.childElementCount) {
+			chat.friendList.appendChild(li);
+		}
 
 	},
 	// change the active user for another one;
 	switchToUser: (friendId) => {
-	    
-	    // We store the current user info in case we come back to him
-	    let map = chat.chatMessage[chat.activeFriend];
-	    map.lastActivePage = chat.currentMessagePage;
-	    map.needMoreLoading = chat.loadPrevious;
-	    map.scroll = chat.scrollableDiv.scrollTop;
-	    
-	    // Cleans chat screen
-	    chat.currentMessagePage = 0;
-	    while (chat.conversation.lastChild) {
-			chat.conversation.lastChild.remove();
-	    }
-	    // Prepares for switch
-	    let newMap = chat.chatMessage[friendId];
-	    chat.activeFriend = friendId;
-	    // If we dont have information about the user we fetch it;
-	    if (!newMap) {
-		newMap  = chat.chatMessage[friendId] = { messages: [], scroll: 0,needMoreLoading: false,lastActivePage:0};
-		apis.mockUpChats();
-	    }
-	    else {
-	    messages.loadExistingMessages(newMap.messages);
-	    }
-	    chat.curretMessagePage = newMap.lastActivePage;
-	    chat.loadPrevious = newMap.needMoreLoading;
-	    chat.scrollableDiv.scrollTop = newMap.scroll;
-	},
 
+		// We store the current user info in case we come back to him
+		let map = chat.chatMessage[chat.activeFriend];
+		map.lastActivePage = chat.currentMessagePage;
+		map.needMoreLoading = chat.loadPrevious;
+		map.scroll = chat.scrollableDiv.scrollTop;
+
+		// Cleans chat screen
+		chat.currentMessagePage = 0;
+		while (chat.conversation.lastChild) {
+			chat.conversation.lastChild.remove();
+		}
+		// Prepares for switch
+		let newMap = chat.chatMessage[friendId];
+		chat.activeFriend = friendId;
+		// If we dont have information about the user we fetch it;
+		if (!newMap) {
+			newMap = chat.chatMessage[friendId] = {
+				messages: [],
+				scroll: 0,
+				needMoreLoading: false,
+				lastActivePage: 0
+			};
+			apis.mockUpChats();
+		} else {
+			messages.loadExistingMessages(newMap.messages);
+		}
+		chat.curretMessagePage = newMap.lastActivePage;
+		chat.loadPrevious = newMap.needMoreLoading;
+		chat.scrollableDiv.scrollTop = newMap.scroll;
+	}
 };
 let messages = {
-	
+
 	// Adds user existing MEssages to chat Screen
 	loadExistingMessages: (messageList) => {
 		let documentFragment = document.createDocumentFragment();
@@ -193,26 +237,27 @@ let messages = {
 		chat.conversation.appendChild(documentFragment);
 
 	},
-	
+
 	// Adds received or sent Message to DOM
 	addMessage: (message) => {
 
-	    	let prevScroll = chat.scrollableDiv.scrollHeight;
-	    	var built_message = messages.buildMessage(message);
+		let prevScroll = chat.scrollableDiv.scrollHeight;
+		var built_message = messages.buildMessage(message);
 		chat.conversation.insertAdjacentElement('afterbegin', built_message);
 		let list = chat.chatMessage[chat.activeFriend];
-// if (!list) {
-// list = chat.chatMessage[chat.activeFriend] ={ messages: [], scroll:
-// 0,needMoreLoading: false,lastActivePage:0};
-// }
+		// if (!list) {
+		// list = chat.chatMessage[chat.activeFriend] ={ messages: [],
+		// scroll:
+		// 0,needMoreLoading: false,lastActivePage:0};
+		// }
 		list.messages.unshift(built_message);
-		
+
 		messages.animateMessage(built_message);
-	    	
-		chat.scrollableDiv.scrollTop += (chat.scrollableDiv.scrollHeight-prevScroll);
+
+		chat.scrollableDiv.scrollTop += (chat.scrollableDiv.scrollHeight - prevScroll);
 	},
-	
-	
+
+
 	animateMessage: (message) => {
 		setTimeout(() => {
 			var tick = message.querySelector('.tick');
@@ -221,7 +266,11 @@ let messages = {
 
 	},
 	// From message metadata build element
-	buildMessage: ({text,time,sendByMe}) => {
+	buildMessage: ({
+		text,
+		date,
+		sendByMe
+	}) => {
 		var element = document.createElement('div');
 
 		let classname = (sendByMe) ? "sent" : "received";
@@ -229,7 +278,7 @@ let messages = {
 		element.classList.add('message', classname);
 		element.innerHTML = text +
 			'<span class="metadata">' +
-			'<span class="time">' + time + '</span>' +
+			'<span class="time">' + date + '</span>' +
 			'<span class="tick tick-animation">' +
 			'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck" x="2047" y="2061"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#92a58c"/></svg>' +
 			'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg>' +
@@ -237,15 +286,14 @@ let messages = {
 			'</span>';
 		return element;
 	},
-	
+
 	// When you input a new message
 	newMessage: (e) => {
 		var input = e.target.input;
 		if (input.value) {
 			var message = messages.buildMessage({
 				text: input.value,
-				time: moment()
-					.format('h:mm A'),
+				date: moment().format("HH:mm"),
 				sendByMe: true
 			});
 			let list = chat.chatMessage[chat.activeFriend];
@@ -258,14 +306,15 @@ let messages = {
 	}
 };
 let apis = {
-	
+
 	mockUpFriends: () => {
 		for (i = 0; i < 4; i++) {
 			friends.addFriendToList({
 				id: i,
 				lastMessage: "x",
 				name: i,
-				count: Math.random()
+				count: Math.floor(Math.random() * 100),
+				date: moment()
 			});
 		}
 	},
@@ -277,8 +326,7 @@ let apis = {
 				.substr(0, 5);
 			messages.addMessage({
 				text: rand,
-				time: moment()
-					.format('h:mm A'),
+				date: chat.formatDate(moment()),
 				sendByMe: i % 2
 			});
 		}
