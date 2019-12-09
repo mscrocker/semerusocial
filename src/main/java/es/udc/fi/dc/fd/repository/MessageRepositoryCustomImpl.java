05304e5990d2641e6b81584b889748532bd0046d
Ishwara Coello Escobar <ishwara.coello@udc.es>
2019-12-09 18:36:22 +0100

    #392 Resolving Thread 1,2
    This commit refs #392 @10m
    Task-Url: https://redmine.fic.udc.es/issues/392

--- src/main/java/es/udc/fi/dc/fd/repository/MessageRepositoryCustomImpl.java
+++ src/main/java/es/udc/fi/dc/fd/repository/MessageRepositoryCustomImpl.java
@@ -24,114 +24,109 @@ public class MessageRepositoryCustomImpl implements MessageRepositoryCustom {
 	public Slice<FriendChatTitle> getLatestConversations(Long userId, int page, int size) {
 		//@formatter:off
 
-		Query q = entityManager.createNativeQuery(
-			" Select main.friendId, u.userName,main.content,main.sentByYou,main.date\r\n" +
-			"from \r\n" +
-			" (\r\n" +
-			" (\r\n" +
-			"  SELECT \r\n" +
-			"    CASE\r\n" +
-			"      WHEN conversation.u2 = :userId THEN conversation.u1\r\n" +
-			"      ELSE conversation.u2\r\n" +
-			"    END friendId, \r\n" +
-			"    t.messagecontent content, \r\n" +
-			"    (transmitter = :userId) sentByYou, \r\n" +
-			"    conversation.hora date\r\n" +
-			"  FROM (\r\n" +
-			"    SELECT \r\n" +
-			"      user1 u1, \r\n" +
-			"      user2 u2, \r\n" +
-			"      max(date) hora\r\n" +
-			"    FROM MessageTable m\r\n" +
-			"    WHERE (\r\n" +
-			"      user1 = :userId\r\n" +
-			"      OR user2 = :userId\r\n" +
-			"    )\r\n" +
-			"    GROUP BY \r\n" +
-			"      user1, \r\n" +
-			"      user2\r\n" +
-			"    \r\n" +
-			"  ) conversation\r\n" +
-			"    JOIN MessageTable t\r\n" +
-			"      ON (\r\n" +
-			"        t.date = conversation.hora\r\n" +
-			"        AND user1 = conversation.u1\r\n" +
-			"        AND user2 = conversation.u2\r\n" +
-			"      )	\r\n" +
-			")\r\n" +
-			"UNION ALL (\r\n" +
-			"  SELECT \r\n" +
-			"    CASE\r\n" +
-			"      WHEN user1 = :userId THEN user2\r\n" +
-			"      ELSE user1\r\n" +
-			"    END friendId, \r\n" +
-			"    NULL, \r\n" +
-			"    FALSE, \r\n" +
-			"    NULL\r\n" +
-			"  FROM matchtable m\r\n" +
-			"  WHERE (\r\n" +
-			"    (\r\n" +
-			"      m.user1 = :userId\r\n" +
-			"      AND m.user2 NOT IN ((\r\n" +
-			"        SELECT CASE\r\n" +
-			"          WHEN user1 = :userId THEN user2\r\n" +
-			"          ELSE user1\r\n" +
-			"        END\r\n" +
-			"        FROM MessageTable\r\n" +
-			"        WHERE (\r\n" +
-			"          user1 = :userId\r\n" +
-			"          OR user2 = :userId\r\n" +
-			"        )\r\n" +
-			"        GROUP BY \r\n" +
-			"          user1, \r\n" +
-			"          user2\r\n" +
-			"        \r\n" +
-			"      ))\r\n" +
-			"    )\r\n" +
-			"    OR (\r\n" +
-			"      m.user1 NOT IN (\r\n" +
-			"        SELECT CASE\r\n" +
-			"          WHEN user1 = :userId THEN user2\r\n" +
-			"          ELSE user1\r\n" +
-			"        END\r\n" +
-			"        FROM MessageTable\r\n" +
-			"        WHERE (\r\n" +
-			"          user1 = :userId\r\n" +
-			"          OR user2 = :userId\r\n" +
-			"        )\r\n" +
-			"        GROUP BY \r\n" +
-			"          user1, \r\n" +
-			"          user2\r\n" +
-			"        \r\n" +
-			"      )\r\n" +
-			"      AND m.user2 = :userId\r\n" +
-			"    )\r\n" +
-			"  )\r\n" +
-			")\r\n" +
-			") main\r\n" +
-			"join UserTable u \r\n" +
-			"on u.id = main.friendId\r\n" +
-			"ORDER BY date DESC\r\n" +
-			"LIMIT :size\r\n" +
-			"OFFSET :page");
+		final Query q = entityManager.createNativeQuery(
+				" Select main.friendId, u.userName,main.content,main.sentByYou,main.date\r\n" +
+						"from \r\n" +
+						" (\r\n" +
+						" (\r\n" +
+						"  SELECT \r\n" +
+						"    CASE\r\n" +
+						"      WHEN conversation.u2 = :userId THEN conversation.u1\r\n" +
+						"      ELSE conversation.u2\r\n" +
+						"    END friendId, \r\n" +
+						"    t.messagecontent content, \r\n" +
+						"    ( case when (transmitter = :userId) then 'true' else 'false' end) sentByYou, \r\n" +
+						"    conversation.hora date\r\n" +
+						"  FROM (\r\n" +
+						"    SELECT \r\n" +
+						"      user1 u1, \r\n" +
+						"      user2 u2, \r\n" +
+						"      max(date) hora\r\n" +
+						"    FROM MessageTable m\r\n" +
+						"    WHERE (\r\n" +
+						"      user1 = :userId\r\n" +
+						"      OR user2 = :userId\r\n" +
+						"    )\r\n" +
+						"    GROUP BY \r\n" +
+						"      user1, \r\n" +
+						"      user2\r\n" +
+						"    \r\n" +
+						"  ) conversation\r\n" +
+						"    JOIN MessageTable t\r\n" +
+						"      ON (\r\n" +
+						"        t.date = conversation.hora\r\n" +
+						"        AND user1 = conversation.u1\r\n" +
+						"        AND user2 = conversation.u2\r\n" +
+						"      )	\r\n" +
+						")\r\n" +
+						"UNION ALL (\r\n" +
+						"  SELECT \r\n" +
+						"    CASE\r\n" +
+						"      WHEN user1 = :userId THEN user2\r\n" +
+						"      ELSE user1\r\n" +
+						"    END friendId, \r\n" +
+						"    NULL, \r\n" +
+						"    FALSE, \r\n" +
+						"    NULL\r\n" +
+						"  FROM MatchTable m\r\n" +
+						"  WHERE (\r\n" +
+						"    (\r\n" +
+						"      m.user1 = :userId\r\n" +
+						"      AND m.user2 NOT IN ((\r\n" +
+						"        SELECT CASE\r\n" +
+						"          WHEN user1 = :userId THEN user2\r\n" +
+						"          ELSE user1\r\n" +
+						"        END\r\n" +
+						"        FROM MessageTable\r\n" +
+						"        WHERE (\r\n" +
+						"          user1 = :userId\r\n" +
+						"          OR user2 = :userId\r\n" +
+						"        )\r\n" +
+						"        GROUP BY \r\n" +
+						"          user1, \r\n" +
+						"          user2\r\n" +
+						"        \r\n" +
+						"      ))\r\n" +
+						"    )\r\n" +
+						"    OR (\r\n" +
+						"      m.user1 NOT IN (\r\n" +
+						"        SELECT CASE\r\n" +
+						"          WHEN user1 = :userId THEN user2\r\n" +
+						"          ELSE user1\r\n" +
+						"        END\r\n" +
+						"        FROM MessageTable\r\n" +
+						"        WHERE (\r\n" +
+						"          user1 = :userId\r\n" +
+						"          OR user2 = :userId\r\n" +
+						"        )\r\n" +
+						"        GROUP BY \r\n" +
+						"          user1, \r\n" +
+						"          user2\r\n" +
+						"        \r\n" +
+						"      )\r\n" +
+						"      AND m.user2 = :userId\r\n" +
+						"    )\r\n" +
+						"  )\r\n" +
+						")\r\n" +
+						") main\r\n" +
+						"join UserTable u \r\n" +
+						"on u.id = main.friendId\r\n" +
+						"ORDER BY date DESC\r\n" +
+						"LIMIT :size\r\n" +
+				"OFFSET :page");
 		//@formatter:on
 
 		q.setParameter("userId", userId);
 		q.setParameter("size", size + 1);
 		q.setParameter("page", page * size);
 
-		List<FriendChatTitle> result = ((List<Object[]>) q.getResultList()).stream().map((Object[] e) -> {
-			Boolean e3;
-			try {
-				e3 = ((Integer) e[3]).equals(0) ? false : true;
-			} catch (ClassCastException ex) {
-				e3 = (Boolean) e[3];
-			}
+		final List<FriendChatTitle> result = ((List<Object[]>) q.getResultList()).stream().map((Object[] e) -> {
 			return new FriendChatTitle(((BigInteger) e[0]).longValue(), (String) e[1],
-					e[2] == null ? "" : (String) e[2], e3, e[4] == null ? null : ((Timestamp) e[4]).toLocalDateTime());
+					e[2] == null ? "" : (String) e[2], e[3].equals("true"),
+					e[4] == null ? null : ((Timestamp) e[4]).toLocalDateTime());
 		}).collect(Collectors.toList());
 
-		boolean hasNext = result.size() == size + 1;
+		final boolean hasNext = result.size() == size + 1;
 		if (hasNext) {
 			result.remove(result.size() - 1);
 		}
