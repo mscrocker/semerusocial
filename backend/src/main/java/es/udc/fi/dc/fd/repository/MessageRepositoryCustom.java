ad70fe6eb89b7aa7f24dbe2c5bc64e0f7e4fc965
mirian.martinez.alvarez <mirian.martinez.alvarez@udc.es>
2019-12-20 14:36:00 +0100

    395: Module Subdivision
    Task-Url: https://redmine.fic.udc.es/issues/395
    
    This commit refs #395 @6h
    
    División de la aplicación en dos módulos, uno para el backend con los
    servicios REST y otro para el frontend que lo consume.

---
+++ backend/src/main/java/es/udc/fi/dc/fd/repository/MessageRepositoryCustom.java
@@ -0,0 +1,10 @@
+package es.udc.fi.dc.fd.repository;
+
+import org.springframework.data.domain.Slice;
+
+import es.udc.fi.dc.fd.model.FriendChatTitle;
+
+public interface MessageRepositoryCustom {
+	
+	Slice<FriendChatTitle> getLatestConversations(Long userId, int page, int size);
+}
